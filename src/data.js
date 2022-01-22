09b16b90a350e2cb56344f25909e02b8d5ede6b5
Draqode <Boomjay008@gmail.com>
2022-01-22 09:12:39 +0100

    hid the bt

--- src/data.js
+++ src/data.js
@@ -1,5 +1,6 @@
+const {REACT_APP_BEARER_TOKEN} = process.env
 var myHeaders = new Headers();
-myHeaders.append("Authorization", "Bearer AAAAAAAAAAAAAAAAAAAAAIn6VwEAAAAAmyqmpahr101R1i42GPCmwx33zAA%3DKzNTHZpo14SgSRuIQwUjBAomq1varYU0O1b8rDxtccc7ZarCOs");
+myHeaders.append("Authorization", `${REACT_APP_BEARER_TOKEN}`);
 myHeaders.append("Cookie", "guest_id=v1%3A164229841681311547; guest_id_ads=v1%3A164229841681311547; guest_id_marketing=v1%3A164229841681311547; personalization_id=\"v1_J6y25CTLm2vfZTLiTiOH/w==\"");
 
 var requestOptions = {
